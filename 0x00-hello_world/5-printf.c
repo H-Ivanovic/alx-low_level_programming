d08a3395fcbc14d284ad8503b77e04ad53309e77
H-Ivanovic <hamalaivan808@gmailcom>
2022-09-08 14:44:19 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#incude<stdio.h>
+
+/**
+ * main-prints a text
+ *
+ * Description: using the main function
+ * this program prints "with proper grammar, but the outcome is a piece of art
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
