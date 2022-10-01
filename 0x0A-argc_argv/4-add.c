7a396761fbfaa3d9b842358e79c8a399f4258ce6
H-Ivanovic <hamalaivan808@gmailcom>
2022-09-30 18:15:14 -0700

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -26,3 +26,35 @@ count++;
 }
 return (1);
 }
+
+/**
+ * main-prints the program
+ * @argv:argument
+ * @argc:count argument
+ *
+ * Return:0 (success)
+ */
+int main(int argc, char *argv[])
+{
+int count;
+int str_to_int;
+int sum = 0;
+
+count = 1;
+while (count < argc)
+{
+if (check_num(argv[count]))
+{
+str_to_int = atoi(argv[count]);
+sum += str_to_int;
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+count++;
+}
+printf("%d\n", sum);
+return (0);
+}
