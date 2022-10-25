4bb830126eb8b81d0a5556148cb400232418bdf8
H-Ivanovic <hamalaivan808@gmailcom>
2022-10-25 13:06:47 -0700

    1. List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len-returns the number of elements
+ * @h:pointer
+ *
+ * Return:number of elememts
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
+
