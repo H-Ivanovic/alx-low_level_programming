093677e6c12e7431db55809e988c33f501fed5dc
H-Ivanovic <hamalaivan808@gmail.com>
2023-04-18 02:51:43 -0700

    task

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,28 +1,27 @@
 #include "lists.h"
 
 /**
- * reverse_listint-reverses a listint_t linked list.
- * @head:pointer to the head
+ * reverse_listint - reverses a linked list.
+ * @head: head of a list.
  *
- * Return:pointer to the first node of the reversed list
+ * Return: pointer to the first node.
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *head, *behind;
+	listint_t *p;
+	listint_t *n;
 
-	if (head == NULL || *head == NULL)
-		return (NULL);
+	p = NULL;
+	n = NULL;
 
-	behind = NULL;
-
-	while ((*head)->next != NULL)
+	while (*head != NULL)
 	{
-		ahead = (*head)->next;
-		(*head)->next = behind;
-		behind = *head;
-		*head = ahead;
+		n = (*head)->next;
+		(*head)->next = p;
+		p = *head;
+		*head = n;
 	}
 
-	(*head)->next = behind;
-
+	*head = p;
 	return (*head);
+}
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,24 +1,32 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end- adds a new node at the end
- * @head:pointer
- * @n:integer
+ * add_nodeint_end - adds a node to the end of a linked list
+ * @head: pointer to the head of the list
+ * @n: number to be used as content
  *
- * Return:the address of the new element
+ * Return: address of the newly added node
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new;
+	listint_t *new_node;
+	listint_t *cursor = *head;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node != NULL)
+	{
+		new_node->n = n;
+		new_node->next = NULL;
+	}
+	else
 		return (NULL);
-
-	new->n = n;
-	new->next = *head;
-
-	*head = new;
-
-	return (new);
+	if (cursor != NULL)
+	{
+		while (cursor->next != NULL)
+			cursor = cursor->next;
+		cursor->next = new_node;
+	}
+	else
+		*head = new_node;
+	return (new_node);
 }
